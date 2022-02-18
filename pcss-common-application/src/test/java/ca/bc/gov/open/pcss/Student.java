3f3fd88aec0f4d64f22f8af80f0f01d29b934893
jianming tu <tu_jm@yahoo.com>
2022-02-18 13:58:03 -0800

    add error

---
+++ pcss-common-application/src/test/java/ca/bc/gov/open/pcss/Student.java
@@ -0,0 +1,16 @@
+package ca.bc.gov.open.pcss;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+    protected String param1;
+
+    public String getParam1() {
+        return param1;
+    }
+
+    public void setParam1(String value) {
+        this.param1 = value;
+    }
+}
