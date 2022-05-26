8f926c01302534c5388aeb7b2a379b027b97ae10
Sylar Z. Zhang <Sylar.Zhang@gov.bc.ca>
2022-05-26 13:47:01 -0700

    Revert "added agenId and partId validation in codevalue secure call"
    This reverts commit ff1fce3c700ecadc71234c9578205e63c3d3bc14.

--- pcss-common-application/src/main/java/ca/bc/gov/open/pcss/controllers/SecureEndpointController.java
+++ pcss-common-application/src/main/java/ca/bc/gov/open/pcss/controllers/SecureEndpointController.java
@@ -28,12 +28,6 @@ public class SecureEndpointController {
     @Value("${pcss.host}")
     private String host = "https://127.0.0.1/";
 
-    @Value("${pcss.generic-agent-id}")
-    private String genericAgenId = "";
-
-    @Value("${pcss.generic-part-id}")
-    private String genericPartId = "";
-
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
 
@@ -113,14 +107,8 @@ public class SecureEndpointController {
                         ? search.getGetCodeValuesSecureRequest().getGetGetCodeValueSecureRequest()
                         : new ca.bc.gov.open.wsdl.pcss.secure.one.GetGetCodeValueSecureRequest();
 
-        String secureExt =
-                inner.getRequestAgencyIdentifierId().equals(genericAgenId)
-                                && inner.getRequestPartId().equals(genericPartId)
-                        ? ""
-                        : "secure";
-
         UriComponentsBuilder builder =
-                UriComponentsBuilder.fromHttpUrl(host + secureExt + "/code-values")
+                UriComponentsBuilder.fromHttpUrl(host + "secure/code-values")
                         .queryParam("requestAgencyId", inner.getRequestAgencyIdentifierId())
                         .queryParam("requestPartId", inner.getRequestPartId())
                         .queryParam("requestDtm", InstantSerializer.convert(inner.getRequestDtm()))
